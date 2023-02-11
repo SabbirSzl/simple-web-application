cbaa5ac35265913c2f39447497939f66df23a008
Sabbir Ahmed Sozol <sabbir.sozol@northsouth.edu>
2023-02-12 02:16:10 +0600

    added Registration Functionalities

--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-registration',
@@ -6,5 +9,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+
+  registrationForm:any = FormGroup;
+  responseMessage:any;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: Router,
+    private userservice: UserService
+  )
+  {
+
+  }
+ngOnInit():void{
+  this.registrationForm = this.formBuilder.group({
+    firstname:[null,[Validators.required]],
+    lastname:[null,[Validators.required]],
+    email:[null,[Validators.required]],
+    password:[null,[Validators.required]]
+  })
+}
+  registrationClick()
+  {
+    var formData = this.registrationForm.value;
+    var data = {
+    }
+  }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+//import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  //url = environment.apiUrl;
+
+
+  constructor( private httpclient: HttpClient) { }
+
+
+  registration(data:any){
+    /*return this.httpclient.post(this.url+"/user/registration",data,
+    {
+      headers: new HttpHeaders().set('Content-Type',"application/json")
+    })*/
+  }
 }
