6af2944ec941218595d68af72ffaba56300b0fb3
Sabbir Ahmed Sozol <sabbir.sozol@northsouth.edu>
2023-02-13 02:13:04 +0600

    user login & admin login code added

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -8,26 +8,48 @@ import { UserService } from '../services/user.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit{
+export class LoginComponent implements OnInit {
 
-  loginForm:any = FormGroup;
-  errormsg:any;
-  successmsg:any;
+  loginForm: any = FormGroup;
+  errormsg: any;
+  successmsg: any;
   users: any;
 
 
-  constructor (private formBuilder: FormBuilder,
+  constructor(private formBuilder: FormBuilder,
     private activateRouter: ActivatedRoute,
     private router: Router,
-    private userservice: UserService){
+    private userservice: UserService) {
+    localStorage.clear()
   }
 
-ngOnInit(): void {
-  
-}
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
+  }
 
-  logInFunction()
-  {
-    
+  logInFunction() {
+    if (!this.loginForm.valid) {
+      return;
+    }
+    if (this.loginForm.valid) {
+
+      if (this.loginForm.value.email == 'admin@gmail.com') {
+        this.userservice.loginUser(this.loginForm.value).subscribe((res) => {
+          console.log(res, 'Logged In as Admin');
+          //this.loginForm.reset();
+          this.router.navigate(['profile']);
+        })
+      }
+      else {
+        this.userservice.loginUser(this.loginForm.value).subscribe((res) => {
+          console.log(res,'Logged In');
+          //this.loginForm.reset();
+          this.router.navigate(['home']);
+        })
+      }
+    }
   }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -9,16 +9,22 @@ import { Observable, observable } from 'rxjs';
 export class UserService {
 
 
-  constructor( private _http: HttpClient) { }
+  constructor(private _http: HttpClient) { }
 
   apiUrl = 'http://localhost:8080/user';
 
   //get all the data
-  getAllData():Observable<any>{
-  return this._http.get(`${this.apiUrl}/profile`);
+  getAllData(): Observable<any> {
+    return this._http.get(`${this.apiUrl}/profile`);
   }
-// REgister new user
-  registerNewUser(data: any): Observable<any>{
+  // REgister new user
+  registerNewUser(data: any): Observable<any> {
     return this._http.post(`${this.apiUrl}/registration`, data);
   }
+
+  //login validation
+  loginUser(data: any): Observable<any> {
+    return this._http.post(`${this.apiUrl}/login`, data);
+  }
+
 }
