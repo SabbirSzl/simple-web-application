d6b6ca3027b4c059e45e79a8df33ab0c67d7f517
Sabbir Ahmed Sozol <sabbir.sozol@northsouth.edu>
2023-02-13 01:09:06 +0600

    Login page Design Updated

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,16 +1,30 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit{
 
-  constructor (private route: Router){
+  loginForm:any = FormGroup;
+  errormsg:any;
+  successmsg:any;
+  users: any;
 
+
+  constructor (private formBuilder: FormBuilder,
+    private activateRouter: ActivatedRoute,
+    private router: Router,
+    private userservice: UserService){
   }
+
+ngOnInit(): void {
+  
+}
 
   logInFunction()
   {
